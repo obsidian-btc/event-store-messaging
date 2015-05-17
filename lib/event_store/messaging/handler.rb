3060c686348760ff0c594f4bb5175287d1ea26db
Scott Bellware <sbellware@gmail.com>
2015-05-17 00:06:33 -0500

    Registry can be checked for a class that has been registered

--- lib/event_store/messaging/handler.rb
+++ lib/event_store/messaging/handler.rb
@@ -34,12 +34,16 @@ module EventStore
           @message_classes ||= []
         end
 
+        def message_registered?(message_class)
+          message_classes.include? message_class
+        end
+
         def register_message_class(message_class)
           logger = Telemetry::Logger.get self
 
           logger.trace "Registering message class: #{message_class}"
 
-          unless message_classes.include? message_class
+          unless message_registered?(message_class)
             message_classes.push(message_class)
             logger.debug "Registered message class: #{message_class}"
           else
