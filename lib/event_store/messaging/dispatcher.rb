b8bc700f5cd57ef81b34586fee5a7533bf4590cd
Scott Bellware <sbellware@gmail.com>
2015-05-17 00:10:54 -0500

    Duplicated code is removed

--- lib/event_store/messaging/dispatcher.rb
+++ lib/event_store/messaging/dispatcher.rb
@@ -23,31 +23,6 @@ module EventStore
           handlers << handler_class
           register_message_classes(handler_class.message_classes)
         end
-      end
-
-      module MessageRegistry
-        def message_classes
-          @message_classes ||= []
-        end
-
-        def message_registered?(message_class)
-          message_classes.include? message_class
-        end
-
-        def register_message_class(message_class)
-          logger = Telemetry::Logger.get self
-
-          logger.trace "Registering message class: #{message_class}"
-
-          unless message_registered?(message_class)
-            message_classes.push(message_class)
-            logger.debug "Registered message class: #{message_class}"
-          else
-            logger.debug "Message class: #{message_class} is already registered. Not registered again."
-          end
-
-          message_classes
-        end
 
         def register_message_classes(message_classes)
           message_classes.each { |message_class| register_message_class(message_class) }
