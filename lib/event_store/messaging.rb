6fb95ba2b2280cd99d55d438690a715f24c85b87
Scott Bellware <sbellware@gmail.com>
2015-05-16 19:47:45 -0500

    New paths

--- lib/event_store/messaging.rb
+++ lib/event_store/messaging.rb
@@ -1,6 +1,6 @@
 require 'schema'
 require 'telemetry/logger'
 
-require 'messaging/message'
-require 'messaging/handler'
-require 'messaging/dispatcher'
+require 'event_store/messaging/message'
+require 'event_store/messaging/handler'
+require 'event_store/messaging/dispatcher'
--- lib/messaging.rb
+++
@@ -1,6 +0,0 @@
-require 'schema'
-require 'telemetry/logger'
-
-require 'messaging/message'
-require 'messaging/handler'
-require 'messaging/dispatcher'
