6935a76a01f148c3043b1d9891936e74680992ef
Scott Bellware <sbellware@gmail.com>
2015-05-18 16:35:55 -0500

    Messages compose their names and identifiers

--- lib/event_store/messaging/message.rb
+++ lib/event_store/messaging/message.rb
@@ -3,7 +3,26 @@ module EventStore
     module Message
       def self.included(cls)
         cls.send :include, Schema
+        cls.extend MessageInfo
         cls.extend Build
+      end
+
+      def message_name
+        self.class.message_name
+      end
+
+      def message_identifier
+        self.class.message_identifier
+      end
+
+      module MessageInfo
+        def message_name(cls=self)
+          cls.name.split('::').last
+        end
+
+        def message_identifier(cls=self)
+          message_name(cls).gsub(/([^\^])([A-Z])/,'\1_\2').downcase
+        end
       end
 
       module Build
