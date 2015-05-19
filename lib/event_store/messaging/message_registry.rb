a4d905ad4137d55473baeda887ac9197ddfaae70
Scott Bellware <sbellware@gmail.com>
2015-05-19 01:16:37 -0500

    Using items.find rather than registry.find

--- lib/event_store/messaging/message_registry.rb
+++ lib/event_store/messaging/message_registry.rb
@@ -2,7 +2,7 @@ module EventStore
   module Messaging
     class MessageRegistry < Registry
       def get(message_name)
-        find do |message_class|
+        items.find do |message_class|
           message_class.message_name == message_name
         end
       end
