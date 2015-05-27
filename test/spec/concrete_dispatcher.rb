b7ac4bbf032a3cee556abd2603129f3bb4f65758
Scott Bellware <sbellware@gmail.com>
2015-05-27 13:30:05 -0500

    Grammar in test description is corrected

--- test/spec/concrete_dispatcher.rb
+++ test/spec/concrete_dispatcher.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_init'
 
 describe "Dispatcher" do
-  it "Dispatchers messages to handlers that can handle it" do
+  it "Dispatchers messages to handlers that can handle them" do
     dispatcher = EventStore::Messaging::Dispatcher::Concrete.new
     dispatcher.handlers.register Fixtures.handler.class
 
