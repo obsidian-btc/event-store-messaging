dc35583be2d0e9f07f5b8544f325d151872cefe4
Scott Bellware <sbellware@gmail.com>
2015-06-19 17:07:41 -0500

    Obsolete code is removed

--- test/vertx/client_integration/writer.rb
+++ test/vertx/client_integration/writer.rb
@@ -8,12 +8,3 @@ category_name ||= "someStream#{UUID.random.gsub('-', '')}"
 writer = EventStore::Messaging::Writer.build category_name
 
 writer.write message
-
-
-
-# Vertx.set_timer(1_500) do
-#   assert(stream_entries.length == 1)
-#   assert(stream_entries[0].type == "SomeEvent")
-
-#   logger(__FILE__).data stream_entries.inspect
-# end
