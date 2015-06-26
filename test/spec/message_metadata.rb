7b5ad466b29d6cf5748a69fc59cd6178154b43db
Scott Bellware <sbellware@gmail.com>
2015-06-26 02:13:41 -0500

    Metadata builder builds meta data (not yet incorporated into message construction)

--- test/spec/message_metadata.rb
+++ test/spec/message_metadata.rb
@@ -1,26 +1,28 @@
 require_relative 'spec_init'
 
-describe "Message Metadata" do
-  specify "Has message metadata" do
-    message = Fixtures.some_message
+# TODO Replace these sketch tests [Scott, Thu Jun 25 2015]
 
-    message.metadata.id = 'some id'
-    message.metadata.correlation_stream = 'some correlation stream'
-    message.metadata.causation_stream = 'some causation stream'
-    message.metadata.reply_stream = 'some reply stream'
+# describe "Message Metadata" do
+#   specify "Has message metadata" do
+#     message = Fixtures.some_message
+
+#     message.metadata.event_id = 'some id'
+#     message.metadata.correlation_stream = 'some correlation stream'
+#     message.metadata.causation_stream = 'some causation stream'
+#     message.metadata.reply_stream = 'some reply stream'
 
 
-    puts message.metadata.id
-    puts message.metadata.correlation_stream
-    puts message.metadata.causation_stream
-    puts message.metadata.reply_stream
+#     puts message.metadata.event_id
+#     puts message.metadata.correlation_stream
+#     puts message.metadata.causation_stream
+#     puts message.metadata.reply_stream
 
-    # - - -
+#     # - - -
 
-    puts message.metadata.type
-    puts message.metadata.name
+#     puts message.metadata.type
+#     puts message.metadata.name
 
-    puts message.message_type # => SomeMessage
-    puts message.message_name # => some_message
-  end
-end
+#     puts message.message_type # => SomeMessage
+#     puts message.message_name # => some_message
+#   end
+# end
