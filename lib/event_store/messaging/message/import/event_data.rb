cd37d577c73cea56774bcac4eb5a89e25d4e3feb
Scott Bellware <sbellware@gmail.com>
2015-07-31 11:50:47 -0500

    Vestigial code is removed

--- lib/event_store/messaging/message/import/event_data.rb
+++ lib/event_store/messaging/message/import/event_data.rb
@@ -3,8 +3,6 @@ module EventStore
     module Message
       module Import
         module EventData
-          class Error < RuntimeError; end
-
           def self.logger
             @logger ||= Telemetry::Logger.get self
           end
@@ -28,26 +26,3 @@ module EventStore
     end
   end
 end
-
-=begin
-module Deserialize
-  class Error < StandardError; end
-
-  def deserialize(stream_entry)
-    entry_type = stream_entry.type
-    msg_class = message_registry.get(entry_type)
-
-    msg = nil
-    if msg_class
-      msg_data = stream_entry.data
-      raise Error, "No data in stream entry: #{stream_entry.inspect}" unless msg_data
-
-      metadata = stream_entry.metadata
-
-      msg = msg_class.build(msg_data, metadata)
-    end
-
-    return msg
-  end
-end
-=end
