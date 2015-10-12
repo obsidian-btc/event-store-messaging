cb1ad21977cdfa2c8fd9117e41c3929b2c0e1b2c
ntl <nathanladd+github@gmail.com>
2015-10-12 15:47:30 -0500

    Add substitute for Reader

--- lib/event_store/messaging/reader.rb
+++ lib/event_store/messaging/reader.rb
@@ -4,6 +4,36 @@ module EventStore
       def self.http_reader
         Client::HTTP::Reader
       end
+
+      class Substitute
+        attr_reader :messages
+        attr_reader :reader
+
+        dependency :dispatcher, EventStore::Messaging::Dispatcher
+
+        def initialize(reader)
+          @reader = reader
+          @messages = []
+        end
+
+        def self.build
+          reader = Messaging::Reader.new "substituteStream"
+          new reader
+        end
+
+        def add(message)
+          messages << message
+        end
+
+        def start(&supplementary_action)
+          messages.each_with_index do |message, index|
+            number = index + 1
+            event_data = OpenStruct.new :number => number
+            dispatcher.dispatch message, event_data
+            supplementary_action.(message, event_data) if supplementary_action
+          end
+        end
+      end
     end
   end
 end
