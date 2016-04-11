59c8d1767b5910ecd4fa2d9662bbf33dc4a13005
Nathan Ladd <nathan@obsidianexchange.com>
2016-04-11 15:44:55 -0500

    Remove ProcessHostIntegration (Moved to Consumer)

--- lib/event_store/messaging/subscription.rb
+++ lib/event_store/messaging/subscription.rb
@@ -4,20 +4,6 @@ module EventStore
       def self.http_reader
         Client::HTTP::Subscription
       end
-
-      module ProcessHostIntegration
-        def change_connection_scheduler(scheduler)
-          reader.change_connection_scheduler scheduler
-        end
-
-        def start
-          loop do
-            logger.trace "Starting subscription (Stream Name: #{stream_name.inspect})"
-            super
-            logger.debug "Subscription stopped (Stream Name: #{stream_name.inspect})"
-          end
-        end
-      end
     end
   end
 end
