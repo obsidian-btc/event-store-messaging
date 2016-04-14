a6e53c8b44eafe631c6e48b33443a725dfcd94f0
Nathan Ladd <nathan@obsidianexchange.com>
2016-04-14 16:12:33 -0500

    Rename raw_data control to data

--- lib/event_store/messaging/controls/event_data/read.rb
+++ lib/event_store/messaging/controls/event_data/read.rb
@@ -3,10 +3,10 @@ module EventStore
     module Controls
       module EventData
         module Read
-          def self.raw_data(number=nil, time: nil, stream_name: nil, metadata: nil, type: nil, omit_metadata: nil)
+          def self.data(number=nil, time: nil, stream_name: nil, metadata: nil, type: nil, omit_metadata: nil)
             type ||= 'SomeMessage'
 
-            EventStore::Client::HTTP::Controls::EventData::Read.raw_data(
+            EventStore::Client::HTTP::Controls::EventData::Read.data(
               number,
               time: time,
               stream_name: stream_name,
@@ -17,18 +17,18 @@ module EventStore
           end
 
           def self.example(omit_metadata: nil)
-            raw_data = raw_data(omit_metadata: omit_metadata)
+            raw_data = data(omit_metadata: omit_metadata)
 
             Serialize::Read.instance raw_data, Client::HTTP::EventData::Read
           end
 
           module Anomaly
-            def self.raw_data
-              Read.raw_data type: 'SomeUnknownMessage'
+            def self.data
+              Read.data type: 'SomeUnknownMessage'
             end
 
             def self.example
-              Serialize::Read.instance raw_data, Client::HTTP::EventData::Read
+              Serialize::Read.instance data, Client::HTTP::EventData::Read
             end
           end
         end
